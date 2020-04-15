c19caf09a8a915529f22356f36e7ad5c7f5dee44
Chris Daley <Daleys@Chriss-iMac.local>
2020-04-14 20:52:20 -0600

    created a testing seeds file andand I edited the knexfile to reflect the testing database

--- knexfile.js
+++ knexfile.js
@@ -44,7 +44,7 @@ module.exports = {
   testing: {
     client: 'sqlite3',
     connection: {
-      filename: './database/test.db3'
+      filename: './database/testing.db3'
     },
     useNullAsDefault: true,
     migrations: {
