ceb9e813154ac4b656cd06d86bd1b801b0208d08
Pamela Sage <55425044+Psage23@users.noreply.github.com>
2020-04-15 20:49:32 -0700

    Router/users (#3)
    * user router files created and model
    
    * user router CRUD

---
+++ router/users/users-model.js
@@ -0,0 +1,32 @@
+const db = require('../database/dbconfig.js');
+
+module.exports = {
+    find,
+    update,
+    remove,
+    findById
+};
+
+function find() {
+    return db('users')
+        .select('id', 'username')
+}
+
+function update(id, user) {
+    db('users')
+        .where('id')
+        .update(user)
+    return findById(id);
+}
+
+function remove(id) {
+    return db('users')
+        .where({id})
+        .del();
+}
+
+function findById(id) {
+    return db('users')
+        .where({id})
+        .first();
+}
