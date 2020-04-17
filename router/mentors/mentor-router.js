66ff3de75daf2cd5d818451e7c1a534e73538b24
Miley Wright <dishaliekwright@yahoo.com>
2020-04-16 22:48:05 -0400

    added middleware

--- router/mentors/mentor-router.js
+++ router/mentors/mentor-router.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
-const Users = require('./mentor-model');
-const db = require('../../database/dbconfig');
+const db = require('./mentor-model');
+middleware = require('./middleware');
+
+//change to not show password and email
+// GET mentor/
+router.get('/', (req, res) => {
+    db.getMentors()
+    .then(mentors => {
+        res.status(200).json(mentors)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
 
 module.exports = router;
