529313cca47e8d497c4531f4a2b58fec83ac2583
Chris Daley <Daleys@Chriss-iMac.local>
2020-04-23 22:26:31 -0600

    got register to finally work on Postman Login is giving me some probelms

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcryptjs');
 const secrets = require('../config/secret');
 const Users = require('../router/users/users-model');
 const Mentors = require('../router/mentors/mentor-model');
-// ***Pick up here tomorrow, need to change all of the Users into Mentors**
-// ======================================================================
+
+// =================== User Register ========================================
 router.post('/register', (req,res) => {
   let data = req.body;
   const hash = bcrypt.hashSync(data.password,8);
@@ -21,7 +21,7 @@ router.post('/register', (req,res) => {
       res.status(501).json(error);
     });
 });
-// ====================================================================
+// ==================== User Login =========================================
 router.post('/login', (req,res) => {
 
   const { email, password } = req.body;
@@ -41,11 +41,48 @@ router.post('/login', (req,res) => {
       res.status(500).json({ message: 'user error', err });
     });
 });
-// ==========================================================================================
+// ====================== Mentor Register =============================================
+router.post('/register/mentor', (req,res) => {
+  let data = req.body;
+  const hash = bcrypt.hashSync(data.password,8);
+  data.password = hash;
+
+  Mentors.addMentor(data)
+    .then(saved => {
+      const token = genToken(saved);
+      res.status(201).json({ created_mentor: saved, token: token });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(501).json(error);
+    });
+});
+// ========================= Mentor Login ========================================
+router.post('/login/mentor', (req,res) => {
+
+  const { email, password } = req.body;
+
+  Mentors.getMentor({ email })
+    .first()
+    .then(log => {
+      if (log && bcrypt.compareSync(password, log.password)) {
+        req.session.loggedin = true;
+        const token = genToken(log);
+          res.status(200).json({ email: log.email, token: token });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials '});
+      }
+    })
+    .catch (err => {
+      console.log(err);
+      res.status(500).json({ message: 'user error', err });
+    });
+});
+// ==================== User Token  ====================================================
 function genToken(user) {
   const payload = {
-    usersid: users.id,
-    users: users.email
+    usersid: user.id,
+    users: user.email
   };
   
   const options = { expiresIn: '1h' };
@@ -54,4 +91,17 @@ function genToken(user) {
   return token;
 }
 // ==========================================================================================
+// ========================= Mentor Token ========================================
+// function genTokenMentor(mentor) {
+//   const payload = {
+//     usersid: users.id,
+//     users: users.email
+//   };
+  
+//   const options = { expiresIn: '1h' };
+//   const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+//   return token;
+// }
+
 module.exports = router;
