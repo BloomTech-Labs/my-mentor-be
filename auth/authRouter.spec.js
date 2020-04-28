0758bb1db1e731e55f0103c9f462196cb549e89c
Chris Daley <Daleys@Chriss-iMac.local>
2020-04-27 21:57:29 -0600

    got some tests to pass on my mentor routes

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -1,29 +1,64 @@
-// const request = require('supertest');
-// const server = require('../api/server');
-// const db = require('../database/dbconfig');
-// const Mentor = require('../router/mentors/mentor-model');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbconfig');
+const Mentor = require('../router/mentors/mentor-model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// // ============================================================
-// describe('should register a new user', () => {
-//    it('should return a JSON', async () => {
-//       const res = await request(server).post('/api/auth/register')
-//       .send({
-//          "first_name": "Steve",
-//          "last_name": "Rogers",
-//          "city": "Brooklyn",
-//          "state": "New York",
-//          "password": "AllDay",
-//          "email": "Captain@aol.com",
-//          "profession": "boxer"
-//       });
-//       console.log(res.body);
-//       expect(res.type).toEqual('application/json');
-//       expect(res.status).toEqual(201);
-//    })
-// });
-// // ============================================================
-// beforeEach( async () => {
-//    await db ('mentor').truncate();
-// })
+//======= Function to create a User and test my middleware ===
+async function createMentor(email, password) {
+  const mentor = {
+    email: email,
+    password: bcrypt.hashSync(password, 8)
+  };
+  await Mentor.addMentor(mentor)
+}
+// ==================== genToken ==============================
+function genToken(parent) {
+  const payload = {
+    userid: user.id,
+    email: user.email,
+
+    roles: ['Mentor']
+  };
+
+  const options = { expiresIn: '1h' };
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+  return token;
+}
+// ============= Mentor Register Test =================================
+describe('should register a new mentor', () => {
+   it('should return a JSON', async () => {
+      const res = await request(server).post('/api/auth/register/mentor')
+      .send({
+         "first_name": "Natasha",
+         "last_name": "Romanoff",
+         "city": "Budapest",
+         "state": "Hungary",
+         "password": "spider",
+         "email": "widow@aol.com",
+         "profession": "spy"
+      });
+      console.log(res.body);
+      expect(res.type).toEqual('application/json');
+      expect(res.status).toEqual(201);
+   })
+});
+// ================= Mentor Login ================================
+describe('POST /login', () => {
+  it('should accept valid credentials', async () => {
+      await createMentor("Hawkeye","arrow");
+      const res = await request(server).post('/api/auth/login/mentor')
+      .send({
+          "email": "Hawkeye",
+          "password": "arrow"
+      });
+      console.log(res.body);
+      expect(res.type).toEqual('application/json');
+      expect(res.status).toEqual(200);
+  });
+}); 
+beforeEach( async () => {
+   await db ('mentor').truncate();
+})
--- database/seeds/001-mentors.js
+++ database/seeds/001-mentors.js
@@ -5,7 +5,8 @@ exports.seed = function(knex) {
         return knex('mentor').insert([
           {id: 1, first_name: 'Jane', last_name: 'Doe', city: 'Queens', state: 'New York', email: 'janedoe@email.com', password: 'janedoe', profession: 'UX Designer', image: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: 'Hello, my name is Jane Doe. I\'m a UX Designer from New York, and have been in the industry for over 6 years. I specialize in Graphic Design and am passionate about sharing everything that I \'ve learned on my journey over the last 6 years.Ultimately, my goal is to help teach you everything about designing.'},
           {id: 2, first_name: 'Lucy', last_name: 'Lee', city: 'Atlanta', state: 'Georgia', email: 'lucylee@gmail.com', password: 'lucylee', profession: 'Graphic Designer', image: 'https://images.unsplash.com/photo-1541779408-c355f91b42c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''},
-          {id: 3, first_name: 'Zac', last_name: 'Burns', city: 'Bronx', state: 'New York', email: 'zacburns@email.com', password: 'zacburns', profession: 'UX Designer', image: 'https://images.unsplash.com/photo-1525748822304-6807cb1348ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''}
+          {id: 3, first_name: 'Zac', last_name: 'Burns', city: 'Bronx', state: 'New York', email: 'zacburns@email.com', password: 'zacburns', profession: 'UX Designer', image: 'https://images.unsplash.com/photo-1525748822304-6807cb1348ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''},
+          {id: 9, first_name: 'Clint', last_name: 'Barton', city: 'Harlem', state: 'New York', email: 'Hawkeye', password: '$2a$08$JO1fjQc5QUkWyt1b3M7HIOzQfd13S.6oWn/ZxjFieZO5RPvb3pLve', profession: 'Long Range'}
         ]);
       });
   };
