136b46a21178de5ab87196de1cbb669201e7370d
Miley Wright <dishaliekwright@yahoo.com>
2020-04-29 13:23:58 -0400

    mentee testings complete

--- router/mentees/mentee.spec.js
+++ router/mentees/mentee.spec.js
@@ -22,10 +22,6 @@ const incomplete_creds = {
     description: 'test',
     password: 'test'
 }
-const good_login = {
-    email: 'test',
-    password: 'test'
-}
 
 const updated_user_creds = {
     first_name: 'testuser',
@@ -37,6 +33,7 @@ const updated_user_creds = {
     email: 'tester',
     password: 'tester'
 }
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2Vyc2lkIjoxLCJ1c2VycyI6InRlc3QiLCJpYXQiOjE1ODgxODAzNDMsImV4cCI6MTU4ODE4Mzk0M30.v6zHZxjd2jRmqBprhWDDbmST0BnZnnsvD-CyVjEdnQQ'
 
 beforeEach(async() => {
     await db('mentee').truncate;
@@ -70,67 +67,77 @@ describe('Mentee Tests', () => {
             expect(res.status).toBe(expectedStatusCode)
         })
     })
-    // describe('GET /api/mentee', () => {
-    //     it('Returns all mentees', async() => {
-    //         const expectedStatusCode = 200;
-    //         const res = await request(server)
-    //             .get('/api/mentee')
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    //     it('Returns a JSON', async() => {
-    //         const res = await request(server)
-    //             .get('/api/mentee')
-    //         expect(res.type).toMatch(/json/) 
-    //     })
-    // })
-    // describe('GET api/mentee/:id', () => {
-    //     it('Returns all mentees by ID', async() => {
-    //         const expectedStatusCode = 200;
-    //         const res = await request(server)
-    //             .get('/api/mentee/1')
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    //     it('Returns a JSON', async() => {
-    //         const res = await request(server)
-    //             .get('/api/mentee')
-    //             .set('Content-Type', 'application/json')
-    //             .expect('Content-Type', /json/)
-    //     })
-    //     it('Returns an error if mentee does not exist', async() => {
-    //         const expectedStatusCode = 404;
-    //         const res = await request(server)
-    //             .get('/api/mentee/2')
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    // })
-    // describe('PUT api/mentee/:id', () => {
-    //     it('Modifies an existing mentee by ID', async() => {
-    //         const expectedStatusCode = 200;
-    //         const res = await request(server)
-    //             .put('/api/mentee/1')
-    //             .send(updated_user_creds)
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    //     it('Returns an error if mentee does not exist', async() => {
-    //         const expectedStatusCode = 404;
-    //         const res = await request(server)
-    //             .put('/api/mentee/2')
-    //             .send(updated_user_creds)
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    // })
-    // describe('DELETE /api/mentee/:id', () => {
-    //     it('Deletes everything dependent on the mentee', async() => {
-    //         const expectedStatusCode = 200;
-    //         const res = await request(server)
-    //             .delete('/api/mentee/1')
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    //     it('Returns an error if mentee does not exist', async() => {
-    //         const expectedStatusCode = 404;
-    //         const res = await request(server)
-    //             .delete('/api/mentee/5')
-    //         expect(res.status).toBe(expectedStatusCode)
-    //     })
-    // })
+
+    describe('GET /api/mentee', () => {
+        it('Returns all mentees', async() => {
+            const expectedStatusCode = 200;
+            const res = await request(server)
+                .get('/api/mentee')
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+        it('Returns a JSON', async() => {
+            const res = await request(server)
+                .get('/api/mentee')
+                .set('Authorization', token)
+            expect(res.type).toMatch(/json/) 
+        })
+    })
+    describe('GET api/mentee/:id', () => {
+        it('Returns all mentees by ID', async() => {
+            const expectedStatusCode = 200;
+            const res = await request(server)
+                .get('/api/mentee/1')
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+        it('Returns a JSON', async() => {
+            const res = await request(server)
+                .get('/api/mentee')
+                .set('Authorization', token)
+                .set('Content-Type', 'application/json')
+                .expect('Content-Type', /json/)
+        })
+        it('Returns an error if mentee does not exist', async() => {
+            const expectedStatusCode = 404;
+            const res = await request(server)
+                .get('/api/mentee/2')
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+    })
+    describe('PUT api/mentee/:id', () => {
+        it('Modifies an existing mentee by ID', async() => {
+            const expectedStatusCode = 200;
+            const res = await request(server)
+                .put('/api/mentee/1')
+                .send(updated_user_creds)
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+        it('Returns an error if mentee does not exist', async() => {
+            const expectedStatusCode = 404;
+            const res = await request(server)
+                .put('/api/mentee/2')
+                .send(updated_user_creds)
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+    })
+    describe('DELETE /api/mentee/:id', () => {
+        it('Deletes everything dependent on the mentee', async() => {
+            const expectedStatusCode = 200;
+            const res = await request(server)
+                .delete('/api/mentee/1')
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+        it('Returns an error if mentee does not exist', async() => {
+            const expectedStatusCode = 404;
+            const res = await request(server)
+                .delete('/api/mentee/5')
+                .set('Authorization', token)
+            expect(res.status).toBe(expectedStatusCode)
+        })
+    })
 })
