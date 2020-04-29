b9b792116bd0b0d080be3feebab4b6756e41b0c0
Miley Wright <dishaliekwright@yahoo.com>
2020-04-29 17:16:50 -0400

    Updating the mentor/mentee testing (#11)
    * Reworking mentee tests
    
    * mentee testings complete
    
    * mentor testing completed

--- router/mentees/mentee.spec.js
+++ router/mentees/mentee.spec.js
@@ -20,9 +20,9 @@ const incomplete_creds = {
     state: 'test',
     image: 'test',
     description: 'test',
-    email: null,
     password: 'test'
 }
+
 const updated_user_creds = {
     first_name: 'testuser',
     last_name: 'testuser',
@@ -33,49 +33,53 @@ const updated_user_creds = {
     email: 'tester',
     password: 'tester'
 }
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2Vyc2lkIjoxLCJ1c2VycyI6InRlc3QiLCJpYXQiOjE1ODgxODAzNDMsImV4cCI6MTU4ODE4Mzk0M30.v6zHZxjd2jRmqBprhWDDbmST0BnZnnsvD-CyVjEdnQQ'
 
 beforeEach(async() => {
     await db('mentee').truncate;
-})
+}) 
 describe('Mentee Tests', () => {
     it('Finding mentee tests', async () => {
         await knexCleaner.clean(db)
-        expect(1).toBe(1)
+        expect(1).toBe(1);
     })
 
     describe('POST /api/mentee', () => {
         it('Register mentee with incomplete credentials', async() => {
-            const expectedStatusCode = 500;
+            const expectedStatusCode = 501;
             let res = await request(server)
-                .post('/api/mentee')
+                .post('/api/auth/register/mentee')
                 .send(incomplete_creds)
             expect(res.status).toBe(expectedStatusCode)
         })
         it('Register mentee', async() => {
             const expectedStatusCode = 201;
             const res = await request(server)
-                .post('/api/mentee')
+                .post('/api/auth/register/mentee')
                 .send(good_user_creds)
             expect(res.status).toBe(expectedStatusCode)
         })
         it('Registering with the same credentials, expecting failure', async() => {
-            const expectedStatusCode = 500;
+            const expectedStatusCode = 501;
             const res = await request(server)
-                .post('/api/mentee')
+                .post('/api/auth/register/mentee')
                 .send(good_user_creds)
             expect(res.status).toBe(expectedStatusCode)
         })
     })
+
     describe('GET /api/mentee', () => {
         it('Returns all mentees', async() => {
             const expectedStatusCode = 200;
             const res = await request(server)
                 .get('/api/mentee')
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
         it('Returns a JSON', async() => {
             const res = await request(server)
                 .get('/api/mentee')
+                .set('Authorization', token)
             expect(res.type).toMatch(/json/) 
         })
     })
@@ -84,11 +88,13 @@ describe('Mentee Tests', () => {
             const expectedStatusCode = 200;
             const res = await request(server)
                 .get('/api/mentee/1')
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
         it('Returns a JSON', async() => {
             const res = await request(server)
                 .get('/api/mentee')
+                .set('Authorization', token)
                 .set('Content-Type', 'application/json')
                 .expect('Content-Type', /json/)
         })
@@ -96,6 +102,7 @@ describe('Mentee Tests', () => {
             const expectedStatusCode = 404;
             const res = await request(server)
                 .get('/api/mentee/2')
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
     })
@@ -105,6 +112,7 @@ describe('Mentee Tests', () => {
             const res = await request(server)
                 .put('/api/mentee/1')
                 .send(updated_user_creds)
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
         it('Returns an error if mentee does not exist', async() => {
@@ -112,6 +120,7 @@ describe('Mentee Tests', () => {
             const res = await request(server)
                 .put('/api/mentee/2')
                 .send(updated_user_creds)
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
     })
@@ -120,12 +129,14 @@ describe('Mentee Tests', () => {
             const expectedStatusCode = 200;
             const res = await request(server)
                 .delete('/api/mentee/1')
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
         it('Returns an error if mentee does not exist', async() => {
             const expectedStatusCode = 404;
             const res = await request(server)
                 .delete('/api/mentee/5')
+                .set('Authorization', token)
             expect(res.status).toBe(expectedStatusCode)
         })
     })
