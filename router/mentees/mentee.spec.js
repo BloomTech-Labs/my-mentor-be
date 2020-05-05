6951b916655400611106b2279178573a50132b1c
Miley Wright <dishaliekwright@yahoo.com>
2020-05-05 16:32:30 -0400

    Correcting testings (#14)
    * correcting sessions in auth
    
    * mentor testings
    
    * mentee testing
    
    * mentee and mentor testings are both working

--- router/mentees/mentee.spec.js
+++ router/mentees/mentee.spec.js
@@ -13,13 +13,14 @@ const good_user_creds = {
     email: 'test',
     password: 'test'
 }
-const incomplete_creds = {
-    first_name: 'testuser',
-    last_name: 'testuser',
+const second_creds = {
+    first_name: 'test',
+    last_name: 'test',
     city: 'test',
     state: 'test',
     image: 'test',
     description: 'test',
+    email: 'testing',
     password: 'test'
 }
 
@@ -30,10 +31,13 @@ const updated_user_creds = {
     state: 'tester',
     image: 'tester',
     description: 'tester',
-    email: 'tester',
-    password: 'tester'
+    email: 'test',
+    password: 'test'
 }
-token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2Vyc2lkIjoxLCJ1c2VycyI6InRlc3QiLCJpYXQiOjE1ODgxODAzNDMsImV4cCI6MTU4ODE4Mzk0M30.v6zHZxjd2jRmqBprhWDDbmST0BnZnnsvD-CyVjEdnQQ'
+const login ={
+    email: 'test',
+    password: 'test'
+}
 
 beforeEach(async() => {
     await db('mentee').truncate;
@@ -43,101 +47,116 @@ describe('Mentee Tests', () => {
         await knexCleaner.clean(db)
         expect(1).toBe(1);
     })
-
-    describe('POST /api/mentee', () => {
-        it('Register mentee with incomplete credentials', async() => {
-            const expectedStatusCode = 501;
-            let res = await request(server)
-                .post('/api/auth/register/mentee')
-                .send(incomplete_creds)
-            expect(res.status).toBe(expectedStatusCode)
-        })
-        it('Register mentee', async() => {
-            const expectedStatusCode = 201;
-            const res = await request(server)
-                .post('/api/auth/register/mentee')
-                .send(good_user_creds)
-            expect(res.status).toBe(expectedStatusCode)
-        })
-        it('Registering with the same credentials, expecting failure', async() => {
-            const expectedStatusCode = 501;
-            const res = await request(server)
-                .post('/api/auth/register/mentee')
-                .send(good_user_creds)
-            expect(res.status).toBe(expectedStatusCode)
-        })
+    it('Register mentee', async() => {
+        const expectedStatusCode = 201;
+        const res = await request(server)
+            .post('/api/auth/register/mentee')
+            .send(good_user_creds)
+        expect(res.status).toBe(expectedStatusCode)
     })
 
     describe('GET /api/mentee', () => {
         it('Returns all mentees', async() => {
             const expectedStatusCode = 200;
-            const res = await request(server)
-                .get('/api/mentee')
-                .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
+            let res = await request(server)
+             .post('/api/auth/login/mentee')
+             .send(login)
+             token = res.body.token;
+             let response = await request(server)
+                 .get('/api/mentee')
+                 .set('Authorization', token)
+             expect(response.status).toBe(expectedStatusCode)
         })
         it('Returns a JSON', async() => {
-            const res = await request(server)
+            let res = await request(server)
+            .post('/api/auth/login/mentee')
+            .send(login)
+            token = res.body.token;
+            const response = await request(server)
                 .get('/api/mentee')
                 .set('Authorization', token)
-            expect(res.type).toMatch(/json/) 
+            expect(response.type).toMatch(/json/)
         })
     })
     describe('GET api/mentee/:id', () => {
         it('Returns all mentees by ID', async() => {
             const expectedStatusCode = 200;
-            const res = await request(server)
+            let res = await request(server)
+            .post('/api/auth/login/mentee')
+            .send(login)
+            token = res.body.token;
+            const response = await request(server)
                 .get('/api/mentee/1')
                 .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
+            expect(response.status).toBe(expectedStatusCode)
         })
         it('Returns a JSON', async() => {
-            const res = await request(server)
+            let res = await request(server)
+            .post('/api/auth/login/mentee')
+            .send(login)
+            token = res.body.token;
+            await request(server)
                 .get('/api/mentee')
-                .set('Authorization', token)
                 .set('Content-Type', 'application/json')
+                .set('Authorization', token)
                 .expect('Content-Type', /json/)
         })
         it('Returns an error if mentee does not exist', async() => {
             const expectedStatusCode = 404;
-            const res = await request(server)
+            let res = await request(server)
+            .post('/api/auth/login/mentee')
+            .send(login)
+            token = res.body.token;
+            const response = await request(server)
                 .get('/api/mentee/2')
                 .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
+            expect(response.status).toBe(expectedStatusCode)
         })
     })
     describe('PUT api/mentee/:id', () => {
         it('Modifies an existing mentee by ID', async() => {
             const expectedStatusCode = 200;
             const res = await request(server)
+                 .post('/api/auth/login/mentee')
+                 .send(login)
+            token = res.body.token
+            const response = await request(server)
                 .put('/api/mentee/1')
                 .send(updated_user_creds)
                 .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
+            expect(response.status).toBe(expectedStatusCode)
         })
-        it('Returns an error if mentee does not exist', async() => {
-            const expectedStatusCode = 404;
-            const res = await request(server)
-                .put('/api/mentee/2')
-                .send(updated_user_creds)
-                .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
-        })
-    })
+    }) 
     describe('DELETE /api/mentee/:id', () => {
         it('Deletes everything dependent on the mentee', async() => {
             const expectedStatusCode = 200;
-            const res = await request(server)
+            await request(server)
+            .post('/api/auth/register/mentee')
+            .send(second_creds)
+                const res = await request(server)
+                 .post('/api/auth/login/mentee')
+                 .send({   
+                    email: 'testing',
+                    password: 'test'
+                })
+                token = res.body.token
+            const response = await request(server)
                 .delete('/api/mentee/1')
                 .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
+            expect(response.status).toBe(expectedStatusCode)
         })
-        it('Returns an error if mentee does not exist', async() => {
-            const expectedStatusCode = 404;
-            const res = await request(server)
-                .delete('/api/mentee/5')
-                .set('Authorization', token)
-            expect(res.status).toBe(expectedStatusCode)
+        it('Returns a JSON', async() => {
+            let res = await request(server)
+            .post('/api/auth/login/mentee')
+            .send({
+                email: 'testing',
+                password: 'test'
+            })
+            token = res.body.token
+            const response = await request(server)
+                .delete('/api/mentee/2')
+                .set({Authorization: token})
+                expect(response.type).toMatch(/json/)
         })
     })
 })
