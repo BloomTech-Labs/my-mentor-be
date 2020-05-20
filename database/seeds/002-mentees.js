5f98d48a13786f3e7b68bb26966f619bcaf73a87
Miley Wright <dishaliekwright@yahoo.com>
2020-05-20 13:23:20 -0400

    seeds rehashed and working properly

--- database/seeds/002-mentees.js
+++ database/seeds/002-mentees.js
@@ -1,12 +1,12 @@
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
-// const hash = bcrypt.hashSync(process.env.USER01, 10)
+const hashAaron = bcrypt.hashSync(process.env.USER, 10)
 
 exports.seed = function(knex) {
     return knex('mentee').del()
       .then(function () {
         return knex('mentee').insert([
-          {id: 1, first_name: 'Aaron', last_name: 'Smith', city: 'Atlanta', state: 'Georgia', password: 'aaronsmith', email:'aaronsmith@email.com', title: '', image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''}
+          {id: 1, first_name: 'Aaron', last_name: 'Smith', city: 'Atlanta', state: 'Georgia', password: hashAaron, email:'aaronsmith@email.com', title: '', image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''}
         ]);
       });
   };
