16a879b0a1df4a31f0af130638ea5dc3af06b412
Miley Wright <dishaliekwright@yahoo.com>
2020-05-20 12:23:36 -0400

    unhashed seeds for now

--- database/seeds/002-mentees.js
+++ database/seeds/002-mentees.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
     return knex('mentee').del()
       .then(function () {
         return knex('mentee').insert([
-          {id: 1, first_name: 'Aaron', last_name: 'Smith', city: 'Atlanta', state: 'Georgia', password: hash, email:'Aaron.Smith@email.com', title: '', image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''}
+          {id: 1, first_name: 'Aaron', last_name: 'Smith', city: 'Atlanta', state: 'Georgia', password: hash, email:'aaronsmith@email.com', title: '', image: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60', description: ''}
         ]);
       });
   };
